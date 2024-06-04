73bbd9fcd4ee10988867cb43961017f800772c56
LuizTakahashii <luiz.takahashi@grupointegrado.br>
2024-06-04 20:56:09 -0300

    add
    todolist

--- exercicio2.js
+++
@@ -1,7 +0,0 @@
-
-const {somar, subtrair, dividir, multiplicar} = require('./exercicio1');
-
-console.log(somar(5, 5));
-console.log(subtrair(10, 2));
-console.log(dividir(100, 5));
-console.log(multiplicar(3, 4));
--- exercicio4.js
+++
@@ -1,5 +0,0 @@
-const {nome, email, dataNascimento} = require('./exercicio3.js');
-
-console.log(nome);
-console.log(email);
-console.log(dataNascimento);
